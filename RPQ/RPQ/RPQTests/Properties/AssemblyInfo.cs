b3fba81ed92623f735d378c9f2e0699ced33717d
AvaPL <241574@student.pwr.edu.pl>
2020-03-05 20:03:58 +0100

    Added some test classes.

--- RPQ/RPQ/RPQTests/Properties/AssemblyInfo.cs
+++ RPQ/RPQ/RPQTests/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("E0EBCB14-3CB6-48F3-86EB-99E5FF995DDC")]
+[assembly: Guid("5856D37A-1388-4F36-9370-2273A9B7CDEE")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ RPQ/RPQ/RPQTests/TaskReaderTests.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace RpqTests
+{
+    [TestFixture]
+    public class TaskReaderTests
+    {
+        private const string Filepath = @"../../../Data/data10.txt";
+        private StreamReader _fileReader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileReader = new StreamReader(Filepath);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _fileReader.Close();
+        }
+
+        [Test]
+        public void ShouldFindDataFile()
+        {
+            Assert.NotNull(_fileReader.ReadLine());
+        }
+
+        [Test]
+        public void ShouldReadCorrectTaskListLength()
+        {
+            Assert.Fail(); //TODO: Implement.
+        }
+
+        [Test]
+        public void ShouldReadListedTasksCorrectly()
+        {
+            Assert.Fail(); //TODO: Implement.
+                           //TODO(optional): Implement equality members in Task. 
+        }
+    }
+}
