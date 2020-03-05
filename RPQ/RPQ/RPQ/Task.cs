cbb3879ab9db1fc8520a5537822af4744309437c
AvaPL <241574@student.pwr.edu.pl>
2020-03-05 12:08:21 +0100

    Added Task and TaskReader, not tested yet.

---
+++ RPQ/RPQ/RPQ/Task.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace RPQ
+{
+    public class Task
+    {
+        public int ReadyTime { get; }
+        public int PerformTime { get; }
+        public int QuitTime { get; }
+
+        public Task(int readyTime, int performTime, int quitTime)
+        {
+            ReadyTime = readyTime;
+            PerformTime = performTime;
+            QuitTime = quitTime;
+        }
+
+        public static Task Parse(string taskString)
+        {
+            int[] times = taskString.Split(' ').Select(int.Parse).ToArray();
+            return new Task(times[0], times[1], times[2]);
+        }
+    }
+}
