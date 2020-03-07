76fc2e0460dfb73341878a4e8f74400f7ed34a61
agngan <agnieszka.gano@gmail.com>
2020-03-07 17:01:44 +0100

    Fixed parsing.

--- RPQ/RPQ/RPQ/TaskReader.cs
+++ RPQ/RPQ/RPQ/TaskReader.cs
@@ -1,18 +1,25 @@
 ﻿﻿using System.Collections.Generic;
 using System.IO;
+ using System.Text.RegularExpressions;
 
-namespace RPQ
+ namespace RPQ
 {
     public class TaskReader
     {
         public List<Task> ReadTasksFromFile(StreamReader file)
         {
             string buffer;
-            int numberOfTasks = int.Parse(file.ReadLine().Split(' ')[0]);
+            Regex regex = new Regex(@"\s+");
+            int numberOfTasks = int.Parse(regex.Split(ReadLine(file))[0]);
             List<Task> result = new List<Task>(numberOfTasks);
-            while ((buffer = file.ReadLine()) != null)
+            while ((buffer = ReadLine(file)) != null)
                 result.Add(Task.Parse(buffer));
             return result;
+        }
+
+        private static string ReadLine(StreamReader file)
+        {
+            return file.ReadLine()?.Trim();
         }
     }
 }
