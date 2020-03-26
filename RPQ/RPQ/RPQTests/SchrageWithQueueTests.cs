976e69cd5288d5a501b52078c30140489e5be50d
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-03-26 12:27:08 +0100

    Added previously used Schrage algorithm without queues.

--- RPQ/RPQ/RPQTests/SchrageWithQueueTests.cs
+++ RPQ/RPQ/RPQTests/SchrageWithQueueTests.cs
@@ -6,7 +6,7 @@ using RPQ;
 namespace RpqTests
 {
     [TestFixture]
-    public class SchrageTests
+    public class SchrageWithQueueTests
     {
         private static readonly string[] FilePaths =
         {
@@ -16,6 +16,13 @@ namespace RpqTests
             @"../../../Data/data100.txt",
             @"../../../Data/data200.txt",
             @"../../../Data/data500.txt"
+        };
+        
+        private static readonly string[] FilePathsWithInterrupts =
+        {
+            @"../../../Data/data50.txt",
+            @"../../../Data/data100.txt",
+            @"../../../Data/data200.txt"
         };
 
         private static readonly int[] ExpectedResults =
@@ -27,6 +34,13 @@ namespace RpqTests
             6416,
             14822
         };
+        
+        private static readonly int[] ExpectedResultsWithInterrupts =
+        {
+            1492,
+            3070,
+            6398
+        };
 
         [Test]
         public void ShouldGiveTheRightOrderForTwoTasks()
@@ -36,8 +50,7 @@ namespace RpqTests
             TaskReader taskReader = new TaskReader();
             List<Task> tasks = taskReader.ReadTasksFromFile(fileReader);
             List<Task> expectedTasks = new List<Task>() {new Task(84, 13, 103), new Task(219, 5, 276)};
-
-            Assert.AreEqual(expectedTasks, Schrage.Solve(tasks));
+            Assert.AreEqual(expectedTasks, SchrageWithQueue.Solve(tasks));
         }
 
         [Test]
@@ -48,9 +61,22 @@ namespace RpqTests
                 using StreamReader fileReader = new StreamReader(FilePaths[i]);
                 TaskReader taskReader = new TaskReader();
                 List<Task> unorderedTasks = taskReader.ReadTasksFromFile(fileReader);
-                List<Task> orderedTasks = Schrage.Solve(unorderedTasks);
+                List<Task> orderedTasks = SchrageWithQueue.Solve(unorderedTasks);
                 RPQTimes rpqTimes = RPQTimes.Calculate(orderedTasks);
                 Assert.AreEqual(ExpectedResults[i], rpqTimes.GetMaxQuitTime());
+            }
+        }
+        
+        [Test]
+        public void ShouldGiveCorrectMaxQuitTimeWithInterrupts()
+        {
+            for (int i = 0; i < FilePathsWithInterrupts.Length; i++)
+            {
+                using StreamReader fileReader = new StreamReader(FilePathsWithInterrupts[i]);
+                TaskReader taskReader = new TaskReader();
+                List<Task> unorderedTasks = taskReader.ReadTasksFromFile(fileReader);
+                int result = InterruptedSchrage.Solve(unorderedTasks);
+                Assert.AreEqual(ExpectedResultsWithInterrupts[i], result);
             }
         }
     }
