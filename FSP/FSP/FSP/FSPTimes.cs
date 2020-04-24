0620797985715b3aa75306dfd6691910a9659c06
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-04-24 14:13:43 +0200

    Added FSPTimes for calculating max complete time and tests with test data000.txt file.

---
+++ FSP/FSP/FSP/FSPTimes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FSP
+{
+    public class FSPTimes
+    {
+        private FSPTimes(int numberOfMachines, int numberOfTasks)
+        {
+            StartTimes = InitializeTimesArray(numberOfMachines, numberOfTasks);
+            CompleteTimes = InitializeTimesArray(numberOfMachines, numberOfTasks);
+        }
+
+        private static int[][] InitializeTimesArray(int numberOfMachines, int numberOfTasks)
+        {
+            int[][] result = new int[numberOfMachines][];
+            for (int i = 0; i < result.Length; i++)
+                result[i] = new int[numberOfTasks];
+            return result;
+        }
+
+        public int[][] StartTimes { get; }
+        public int[][] CompleteTimes { get; }
+
+        public static FSPTimes Calculate(List<Task> tasks)
+        {
+            int numberOfMachines = tasks.First().PerformTimes.Count;
+            FSPTimes result = new FSPTimes(numberOfMachines, tasks.Count);
+            for (int i = 0; i < numberOfMachines; i++)
+                for (int j = 0; j < tasks.Count; j++)
+                {
+                    result.StartTimes[i][j] = Math.Max(i == 0 ? 0 : result.CompleteTimes[i - 1][j],
+                        j == 0 ? 0 : result.CompleteTimes[i][j - 1]);
+                    result.CompleteTimes[i][j] = result.StartTimes[i][j] + tasks[j].PerformTimes[i];
+                }
+
+            return result;
+        }
+
+        public int GetMaxCompleteTime()
+        {
+            return CompleteTimes.Last().Last();
+        }
+    }
+}
---
+++ FSP/FSP/FSPTests/FSPTimesTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using FSP;
+using NUnit.Framework;
+
+namespace FSPTests
+{
+    [TestFixture]
+    public class FSPTimesTests
+    {
+        [Test]
+        public void ShouldGiveCorrectMaxQuitTimeForTasksFromFile()
+        {
+            using StreamReader fileReader = new StreamReader( @"../../../Data/data000.txt");
+            TaskReader taskReader = new TaskReader();
+            List<Task> tasks = taskReader.ReadTasksFromFile(fileReader);
+            FSPTimes rpqTimes = FSPTimes.Calculate(tasks);
+            Assert.AreEqual(20, rpqTimes.GetMaxCompleteTime());
+        }
+    }
+}
