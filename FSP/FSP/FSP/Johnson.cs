0460fddb1f11d0cd6a52aa1edba72163ffd678c4
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-04-30 17:39:47 +0200

    Cleaned up code.

--- FSP/FSP/FSP/Johnson.cs
+++ FSP/FSP/FSP/Johnson.cs
@@ -5,24 +5,56 @@ namespace FSP
 {
     public class Johnson
     {
+        private int i;
+        private int j;
+        private readonly Task[] permutation;
+        private readonly LinkedList<Task> tasks;
+
+        private Johnson(List<Task> tasks)
+        {
+            this.tasks = new LinkedList<Task>(tasks);
+            permutation = new Task[tasks.Count];
+            i = 0;
+            j = tasks.Count - 1;
+        }
+
         public static FSPTimes Solve(List<Task> tasks)
         {
-            LinkedList<Task> tasksLinkedList = new LinkedList<Task>(tasks);
-            Task[] permutation = new Task[tasks.Count];
-            int i = 0;
-            int j = tasksLinkedList.Count - 1;
-            while (tasksLinkedList.Count > 0)
-            {
-                int minPerformTime = tasksLinkedList.SelectMany(task => task.PerformTimes).Min();
-                Task minByPerformTime = tasksLinkedList.First(task => task.PerformTimes.Contains(minPerformTime));
-                if (minByPerformTime.PerformTimes[0] < minByPerformTime.PerformTimes[1])
-                    permutation[i++] = minByPerformTime;
-                else
-                    permutation[j--] = minByPerformTime;
-                tasksLinkedList.Remove(minByPerformTime);
-            }
+            Johnson johnson = new Johnson(tasks);
+            return johnson.Solve();
+        }
 
+        private FSPTimes Solve()
+        {
+            while (tasks.Count > 0)
+                ProcessTask();
             return FSPTimes.Calculate(permutation.ToList());
+        }
+
+        private void ProcessTask()
+        {
+            int minPerformTime = GetMinPerformTime();
+            Task minByPerformTime = GetMinByPerformTime(minPerformTime);
+            PutTaskIntoPermutation(minByPerformTime);
+            tasks.Remove(minByPerformTime);
+        }
+
+        private int GetMinPerformTime()
+        {
+            return tasks.SelectMany(task => task.PerformTimes).Min();
+        }
+
+        private Task GetMinByPerformTime(int minPerformTime)
+        {
+            return tasks.First(task => task.PerformTimes.Contains(minPerformTime));
+        }
+
+        private void PutTaskIntoPermutation(Task minByPerformTime)
+        {
+            if (minByPerformTime.PerformTimes[0] < minByPerformTime.PerformTimes[1])
+                permutation[i++] = minByPerformTime;
+            else
+                permutation[j--] = minByPerformTime;
         }
     }
 }
