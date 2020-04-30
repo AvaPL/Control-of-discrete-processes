00675bd72fe5998a164367e1e9520e74cc7b8a3b
agngan <ganowicz.agnieszka@gmail.com>
2020-04-30 17:10:19 +0200

    Started Johnson algorithm.

---
+++ FSP/FSP/FSP/Johnson.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FSP
+{
+    public class Johnson
+    {
+        public FSPTimes Solve(List<Task> tasks)
+        {
+            FSPTimes fspTimes = new FSPTimes(2);
+
+            int i = 0;
+            int j = tasks.Count-1;
+            while (tasks.Count>0)
+            {
+                tasks.Select(t=>t.PerformTimes)
+            }
+        }
+    }
+}
