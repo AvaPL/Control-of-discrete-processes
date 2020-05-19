91fe5caae6330e9e204608fc86d889fd2eef388c
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-05-19 18:56:38 +0200

    Moved swap, twist and temperature reduction methods to SimulatedAnnealing class.

--- FSP/FSP/FSP/SimulatedAnnealing.cs
+++ FSP/FSP/FSP/SimulatedAnnealing.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FSP
 {
     public class SimulatedAnnealing
     {
-        private Random random = new Random();
-        private double temperature;
-        private double endTemperature;
-        private int numberOfEpochs;
-        private Func<double, double> reduceTemperature;
-        private Func<List<Task>, List<Task>> changePermutation;
+        private static readonly Random Random = new Random();
+        private readonly Func<List<Task>, List<Task>> changePermutation;
+        private readonly double endTemperature;
+        private readonly int numberOfEpochs;
+        private readonly Func<double, double> reduceTemperature;
 
         private FSPTimes bestPermutationTimes;
         private FSPTimes currentPermutationTimes;
+        private double temperature;
 
         public SimulatedAnnealing(List<Task> tasks, double initialTemperature, double endTemperature,
             int numberOfEpochs, Func<List<Task>, List<Task>> changePermutation, Func<double, double> reduceTemperature)
@@ -37,7 +38,7 @@ namespace FSP
                     FSPTimes newPermutationTimes = FSPTimes.Calculate(newPermutation);
                     if (newPermutationTimes.GetMaxCompleteTime() > currentPermutationTimes.GetMaxCompleteTime())
                     {
-                        double randomValue = random.NextDouble();
+                        double randomValue = Random.NextDouble();
                         if (randomValue >
                             Math.Exp((currentPermutationTimes.GetMaxCompleteTime() -
                                       newPermutationTimes.GetMaxCompleteTime()) / temperature))
@@ -53,6 +54,64 @@ namespace FSP
             }
 
             return bestPermutationTimes;
+        }
+
+        public static Func<List<Task>, List<Task>> Swap()
+        {
+            return Swap;
+        }
+
+        private static List<Task> Swap(List<Task> permutation)
+        {
+            int index1 = Random.Next(0, permutation.Count);
+            int index2 = Random.Next(0, permutation.Count);
+            List<Task> newPermutation = new List<Task>(permutation);
+            newPermutation[index1] = permutation[index2];
+            newPermutation[index2] = permutation[index1];
+            return newPermutation;
+        }
+
+        public static Func<List<Task>, List<Task>> Twist()
+        {
+            return Twist;
+        }
+
+        private static List<Task> Twist(List<Task> permutation)
+        {
+            int start = Random.Next(0, permutation.Count);
+            int end = Random.Next(0, permutation.Count);
+            if (start > end)
+            {
+                int temp = start;
+                start = end;
+                end = temp;
+            }
+
+            IEnumerable<Task> firstPart = permutation.Take(start);
+            IEnumerable<Task> secondPart = permutation.Skip(start).Take(end - start).Reverse();
+            IEnumerable<Task> thirdPart = permutation.Skip(end);
+            return firstPart.Concat(secondPart).Concat(thirdPart).ToList();
+        }
+
+        public static Func<double, double> ReduceTemperatureLinear(double initialTemperature)
+        {
+            double linearCoefficient = initialTemperature / 10e3;
+            return temperature => ReduceTemperatureLinear(temperature, linearCoefficient);
+        }
+
+        private static double ReduceTemperatureLinear(double temperature, double linearCoefficient)
+        {
+            return temperature - linearCoefficient;
+        }
+
+        public static Func<double, double> ReduceTemperatureGeometric(double geometricCoefficient)
+        {
+            return temperature => ReduceTemperatureGeometric(temperature, geometricCoefficient);
+        }
+
+        private static double ReduceTemperatureGeometric(double temperature, double geometricCoefficient)
+        {
+            return geometricCoefficient * temperature;
         }
     }
 }
--- FSP/FSP/FSPTests/SimulatedAnnealingTests.cs
+++ FSP/FSP/FSPTests/SimulatedAnnealingTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using FSP;
 using NUnit.Framework;
 
@@ -21,56 +20,6 @@ namespace FSPTests
             @"../../../Data/data006.txt"
         };
 
-        private static readonly Random random = new Random();
-        
-        private static List<Task> Swap(List<Task> permutation)
-        {
-            int index1 = random.Next(0, permutation.Count);
-            int index2 = random.Next(0, permutation.Count);
-            List<Task> newPermutation = new List<Task>(permutation);
-            newPermutation[index1] = permutation[index2];
-            newPermutation[index2] = permutation[index1];
-            return newPermutation;
-        }
-
-        private static List<Task> Twist(List<Task> permutation)
-        {
-            int start = random.Next(0, permutation.Count);
-            int end = random.Next(0, permutation.Count);
-            if (start > end)
-            {
-                int temp = start;
-                start = end;
-                end = temp;
-            }
-
-            IEnumerable<Task> firstPart = permutation.Take(start);
-            IEnumerable<Task> secondPart = permutation.Skip(start).Take(end - start).Reverse();
-            IEnumerable<Task> thirdPart = permutation.Skip(end);
-            return firstPart.Concat(secondPart).Concat(thirdPart).ToList();
-        }
-
-        private static Func<double, double> ReduceTemperatureLinear(double initialTemperature)
-        {
-            double linearCoefficient = initialTemperature / 10e3;
-            return temperature => ReduceTemperatureLinear(temperature, linearCoefficient);
-        }
-
-        private static double ReduceTemperatureLinear(double temperature, double linearCoefficient)
-        {
-            return temperature - linearCoefficient;
-        }
-
-        private static Func<double, double> ReduceTemperatureGeometric(double geometricCoefficient)
-        {
-            return temperature => ReduceTemperatureGeometric(temperature, geometricCoefficient);
-        }
-        
-        private static double ReduceTemperatureGeometric(double temperature, double geometricCoefficient)
-        {
-            return geometricCoefficient * temperature;
-        }
-
         [Test]
         public void ShouldGiveMaxQuitTimeUsingSwapAndLinearTemperatureReduction()
         {
@@ -81,12 +30,14 @@ namespace FSPTests
                 List<Task> tasks = taskReader.ReadTasksFromFile(fileReader);
                 double initialTemperature = 100;
                 double endTemperature = 5;
-                SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(tasks, initialTemperature, endTemperature, tasks.Count, Swap, ReduceTemperatureLinear(initialTemperature));
+                SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(tasks, initialTemperature,
+                    endTemperature, tasks.Count, SimulatedAnnealing.Swap(),
+                    SimulatedAnnealing.ReduceTemperatureLinear(initialTemperature));
                 FSPTimes fspTimes = simulatedAnnealing.Solve();
                 Console.WriteLine(filePath + ": " + fspTimes.GetMaxCompleteTime());
             }
         }
-        
+
         [Test]
         public void ShouldGiveMaxQuitTimeUsingTwistAndLinearTemperatureReduction()
         {
@@ -97,12 +48,14 @@ namespace FSPTests
                 List<Task> tasks = taskReader.ReadTasksFromFile(fileReader);
                 double initialTemperature = 100;
                 double endTemperature = 5;
-                SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(tasks, initialTemperature, endTemperature, tasks.Count, Twist, ReduceTemperatureLinear(initialTemperature));
+                SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(tasks, initialTemperature,
+                    endTemperature, tasks.Count, SimulatedAnnealing.Twist(),
+                    SimulatedAnnealing.ReduceTemperatureLinear(initialTemperature));
                 FSPTimes fspTimes = simulatedAnnealing.Solve();
                 Console.WriteLine(filePath + ": " + fspTimes.GetMaxCompleteTime());
             }
         }
-        
+
         [Test]
         public void ShouldGiveMaxQuitTimeUsingSwapAndGeometricTemperatureReduction()
         {
@@ -113,12 +66,14 @@ namespace FSPTests
                 List<Task> tasks = taskReader.ReadTasksFromFile(fileReader);
                 double initialTemperature = 100;
                 double endTemperature = 5;
-                SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(tasks, initialTemperature, endTemperature, tasks.Count, Swap, ReduceTemperatureGeometric(0.97));
+                SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(tasks, initialTemperature,
+                    endTemperature, tasks.Count, SimulatedAnnealing.Swap(),
+                    SimulatedAnnealing.ReduceTemperatureGeometric(0.97));
                 FSPTimes fspTimes = simulatedAnnealing.Solve();
                 Console.WriteLine(filePath + ": " + fspTimes.GetMaxCompleteTime());
             }
         }
-        
+
         [Test]
         public void ShouldGiveMaxQuitTimeUsingTwistAndGeometricTemperatureReduction()
         {
@@ -129,7 +84,9 @@ namespace FSPTests
                 List<Task> tasks = taskReader.ReadTasksFromFile(fileReader);
                 double initialTemperature = 100;
                 double endTemperature = 5;
-                SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(tasks, initialTemperature, endTemperature, tasks.Count, Twist, ReduceTemperatureGeometric(0.97));
+                SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(tasks, initialTemperature,
+                    endTemperature, tasks.Count, SimulatedAnnealing.Twist(),
+                    SimulatedAnnealing.ReduceTemperatureGeometric(0.97));
                 FSPTimes fspTimes = simulatedAnnealing.Solve();
                 Console.WriteLine(filePath + ": " + fspTimes.GetMaxCompleteTime());
             }
